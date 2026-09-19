8cf7a02cc22c143b0af1b491d38407dbed3ab839
Marcin Pypeć <marcin.pypec@mytimeplan.com>
2026-09-19 16:28:37 +0200

    disabled notes & uses sections

--- src/consts.ts
+++ src/consts.ts
@@ -2,8 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'marcin-pypec.com';
-export const SITE_DESCRIPTION =
-	'Software engineering, architecture, and practical lessons from building products.';
+export const SITE_DESCRIPTION = 'Thoughts, lessons, and mistakes from building software.';
 
 export const AUTHOR_NAME = 'Marcin Pypec';
 export const LINKEDIN_URL = 'https://www.linkedin.com/in/marcin-pypeć-997706432';
